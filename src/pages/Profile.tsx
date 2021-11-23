cc4d1939126843e25703c77aa8a3911e206df0cf
Largenty <largenty@gmail.com>
2021-11-23 17:10:49 +0100

    update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import Button from "../components/Button";
 
 const Profile = () => {
+  const cvLink = { text: "CV", link: "/cv" };
+  const linkedInLink = { text: "LinkedIn", link: "/linkedIn" };
+  const gamesLink = { text: "Games", link: "/games" };
   return (
     <React.Fragment>
       <div
@@ -33,6 +37,18 @@ const Profile = () => {
             check my cv, my skills / work, my linkedIn or just play at some
             games.
           </p>
+
+          <div className="flex justify-center" style={{ margin: "20px" }}>
+            <div style={{ margin: "0 20px" }}>
+              <Button {...linkedInLink} />
+            </div>
+            <div style={{ margin: "0 20px" }}>
+              <Button {...cvLink} />
+            </div>
+            <div style={{ margin: "0 20px" }}>
+              <Button {...gamesLink} />
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
